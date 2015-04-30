cd898cbc2fe8712e277a45086563c9ba61e7a12c
huledoufo <leixinxue999@126.com>
2015-04-30 21:30:52 +0800

    Merge branch 'master' of https://github.com/androidmy/ECookRooms
    Conflicts:
    	.idea/misc.xml
    	.idea/vcs.xml
    	app/src/main/java/githubtest/qianfeng/com/ecookrooms/MainActivity.java

--- app/src/main/java/fragments/First_Fragment.java
+++ app/src/main/java/fragments/First_Fragment.java
@@ -15,7 +15,7 @@ public class First_Fragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.first_fragment, container);
+        View view = inflater.inflate(R.layout.first_fragment, null);
 
 
         return view;
