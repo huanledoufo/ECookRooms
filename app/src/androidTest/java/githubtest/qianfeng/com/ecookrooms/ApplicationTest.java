a3c11e474b8b515613e7c258cd59532b8191b858
huledoufo <leixinxue999@126.com>
2015-04-30 10:10:38 +0800

    Initial commit

---
+++ app/src/androidTest/java/githubtest/qianfeng/com/ecookrooms/ApplicationTest.java
@@ -0,0 +1,13 @@
+package githubtest.qianfeng.com.ecookrooms;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
